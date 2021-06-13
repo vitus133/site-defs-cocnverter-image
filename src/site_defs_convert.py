3dd6d303ac857c424967bce7a71ef6025daf4165
Vitaly Grinberg <vgrinber@redhat.com>
2021-06-13 09:02:11 +0300

    Add ini parser and template renderer

--- src/site_defs_convert.py
+++ src/site_defs_convert.py
@@ -1,9 +1,25 @@
+# import git
 import yaml
-import git
-import time
+import json
 from jinja2 import Template
+from munch import Munch
+import configparser
+
+
 
 if __name__ == "__main__":
-  print("Executing conversion")
-  while(1):
-    time.sleep(10)
+  config = configparser.ConfigParser()
+  config.read('ini/ClusterDeployment.ini')
+  deployment = Munch.fromDict(config)
+  with open('ini/authorized_keys', 'r') as ak:
+    auth_keys = ak.read()
+  # print(json.dumps(deployment, indent=2))
+  with open('templates/infra_env.yaml.j2', 'r') as tf:
+    infra_env_str = tf.read()
+  infra_env = Template(infra_env_str)
+  # print(infra_env.render(
+  #   deployment=deployment, ssh_auth_keys_str=auth_keys))
+  cd = yaml.safe_load(infra_env.render(
+    deployment=deployment, ssh_auth_keys_str=auth_keys))
+  print(yaml.dump(cd))
+
